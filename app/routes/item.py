0d646cb1afa611c322f88f627a32cfc130f895b5
Maureen Kambua <maureenkambua75@gmail.com>
2025-11-12 09:51:49 +0300

    add filter scripts to fetch api data

--- app/routes/item.py
+++ app/routes/item.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, HTTPException
 from app.schema.item import ItemCreate, ItemResponse
+import requests
 
 router = APIRouter(prefix="/items", tags=["Items"])
 
 items_db = []
 item_id_counter = 1  
 
+
 @router.get("/", response_model=list[ItemResponse])
 def get_items():
     return items_db
+
 
 @router.post("/", response_model=ItemResponse)
 def create_item(item: ItemCreate):
@@ -19,6 +22,7 @@ def create_item(item: ItemCreate):
     items_db.append(item_data)
     return item_data
 
+
 @router.patch("/{item_id}", response_model=ItemResponse)
 def update_item(item_id: int, item: ItemCreate):
     for db_item in items_db:
@@ -27,6 +31,7 @@ def update_item(item_id: int, item: ItemCreate):
             return db_item
     raise HTTPException(status_code=404, detail="Item not found")
 
+
 @router.delete("/{item_id}")
 def delete_item(item_id: int):
     for index, db_item in enumerate(items_db):
@@ -34,3 +39,21 @@ def delete_item(item_id: int):
             items_db.pop(index)
             return {"message": f"Item {item_id} deleted successfully"}
     raise HTTPException(status_code=404, detail="Item not found")
+
+
+@router.get("/filter", response_model=list[ItemResponse])
+def filter_items(max_price: float | None = None, keyword: str | None = None):
+    filtered = items_db
+
+   
+    if max_price is not None:
+        filtered = [item for item in filtered if item["price"] <= max_price]
+    if keyword:
+        keyword = keyword.lower()
+        filtered = [
+            item for item in filtered
+            if keyword in item["name"].lower() or keyword in item["description"].lower()
+        ]
+
+    print("Filtered results:", filtered)
+    return filtered
